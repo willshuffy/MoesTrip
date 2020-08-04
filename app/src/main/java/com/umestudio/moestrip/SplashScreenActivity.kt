b4aab42d37b3e9d02cb9eede4f8cfba7ea2245b9
willshuffy <saputrawilly26@gmail.com>
2020-08-04 08:49:20 +0700

    Splash Screen

---
+++ app/src/main/java/com/umestudio/moestrip/SplashScreenActivity.kt
@@ -0,0 +1,36 @@
+package com.umestudio.moestrip
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        //load animation
+        val text_logo = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top)
+        val image_logo = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom)
+        val text_desk = AnimationUtils.loadAnimation(this, R.anim.icon_splash)
+
+
+        //run animation
+        iv_logo_text.startAnimation(text_logo)
+        iv_logo.startAnimation(image_logo)
+        tv_desk.startAnimation(text_desk)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity,
+                MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 10000)
+    }
+}
